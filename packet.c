d3aeb67d30b4778a7b79d9eeed36ee176bc9fb3e
Benny Baumann <BenBE@geshi.org>
2013-07-25 20:37:16 +0200

    add: Add helpers to send DHCP request ACKs and NAKs

--- packet.c
+++ packet.c
@@ -42,3 +42,74 @@ bool send_offer(struct dhcp_msg *m, struct dhcp_lease *l) {
 
 	return true;
 }
+
+bool send_ack(struct dhcp_msg *m, struct dhcp_lease *l, struct in_addr *r) {
+	uint8_t *buf = malloc(DHCP_MSG_LEN);
+	if(!buf) {
+		dhcpd_error(ENOMEM, 1, "Could not send DHCPOFFER");
+		return false;
+	}
+
+	size_t send_len = 0;
+	uint8_t *options = NULL;
+
+	// ACK
+	dhcp_msg_reply(buf, &options, &send_len, m, DHCPACK);
+
+	memcpy(&l->address, r, sizeof(struct in_addr));
+
+	ARRAY_COPY(DHCP_MSG_F_YIADDR(buf), &l->address, 4);
+
+	dhcp_opt_insert_val(buf, DHCP_MSG_LEN, &send_len, &options, DHCP_OPT_SERVERID, uint32_t, m->sid->sin_addr.s_addr);
+
+	options = dhcp_opt_add_lease(options, &send_len, l);
+
+	*options = DHCP_OPT_END;
+	DHCP_OPT_CONT(options, send_len);
+
+//	if (debug)
+//		msg_debug(&((struct dhcp_msg){.data = buf, .length = send_len }), 1);
+//	err = sendto(w->fd,
+//		send_buffer, send_len,
+//		MSG_DONTWAIT, (struct sockaddr *)&broadcast, sizeof broadcast);
+
+//	if (err < 0) {
+//		dhcpd_error(0, 1, "Could not send DHCPACK");
+		return false;
+//	}
+
+	free(buf);
+
+	return true;
+}
+
+bool send_nak(struct dhcp_msg *m) {
+	uint8_t *buf = malloc(DHCP_MSG_LEN);
+	if(!buf) {
+		dhcpd_error(ENOMEM, 1, "Could not send DHCPOFFER");
+		return false;
+	}
+
+	size_t send_len = 0;
+	uint8_t *options = NULL;
+
+	dhcp_msg_reply(buf, &options, &send_len, m, DHCPNAK);
+
+	options[0] = DHCP_OPT_END;
+	DHCP_OPT_CONT(options, send_len);
+
+//	if (debug)
+//		msg_debug(&((struct dhcp_msg){.data = buf, .length = send_len }), 1);
+//	err = sendto(w->fd,
+//		buf, send_len,
+//		MSG_DONTWAIT, (struct sockaddr *)&broadcast, sizeof broadcast);
+
+//	if (err < 0) {
+//		dhcpd_error(0, 1, "Could not send DHCPNAK");
+		return false;
+//	}
+
+	free(buf);
+
+	return true;
+}
--- packet.h
+++ packet.h
@@ -3,3 +3,5 @@
 #include "dhcp.h"
 
 bool send_offer(struct dhcp_msg *m, struct dhcp_lease *l);
+bool send_ack(struct dhcp_msg *m, struct dhcp_lease *l, struct in_addr *r);
+bool send_nak(struct dhcp_msg *m);
