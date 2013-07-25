ec5187fbf27eb26cb6c78f725be8b708d3636b5d
Benny Baumann <BenBE@geshi.org>
2013-07-25 20:35:13 +0200

    fix: Fix helper function to send DHCP offers

--- packet.c
+++ packet.c
@@ -1,5 +1,10 @@
 #include "packet.h"
 
+#include <stdlib.h>
+#include <errno.h>
+
+#include "error.h"
+
 bool send_offer(struct dhcp_msg *m, struct dhcp_lease *l) {
 	uint8_t *buf = malloc(DHCP_MSG_LEN);
 	if(!buf) {
@@ -12,11 +17,11 @@ bool send_offer(struct dhcp_msg *m, struct dhcp_lease *l) {
 
 	dhcp_msg_reply(buf, &options, &send_len, m, DHCPOFFER);
 
-	ARRAY_COPY(DHCP_MSG_F_YIADDR(send_buffer), &lease.address, 4);
+	ARRAY_COPY(DHCP_MSG_F_YIADDR(buf), &l->address, 4);
 
-	dhcp_opt_insert_val(buf, DHCP_MSG_LEN, &send_len, options, DHCP_OPT_SERVERID, uint32_t, msg->sid->sin_addr);
+	dhcp_opt_insert_val(buf, DHCP_MSG_LEN, &send_len, &options, DHCP_OPT_SERVERID, uint32_t, m->sid->sin_addr.s_addr);
 
-	options = dhcp_opt_add_lease(options, &send_len, &lease);
+	options = dhcp_opt_add_lease(options, &send_len, l);
 
 	*options = DHCP_OPT_END;
 	DHCP_OPT_CONT(options, send_len);
@@ -28,8 +33,12 @@ bool send_offer(struct dhcp_msg *m, struct dhcp_lease *l) {
 //		send_buffer, send_len,
 //		MSG_DONTWAIT, (struct sockaddr *)&broadcast, sizeof broadcast);
 
-//	if (err < 0)
+//	if (err < 0) {
 //		dhcpd_error(errno, 1, "Could not send DHCPOFFER");
+		return false;
+//	}
 
-	return false;
+	free(buf);
+
+	return true;
 }
